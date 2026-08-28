45cc16d5085a55efe26fe0444fd345b14b34a8cc
DoctorAnsem <45246040+DoctorAnsem@users.noreply.github.com>
2026-08-28 19:30:00 +0300

    Fixed Script ID not being properly treated as DWORD

--- DigimonWorld2Tool/DigimonWorld2Tool/FileEditor/DUNGEditor.cs
+++ DigimonWorld2Tool/DigimonWorld2Tool/FileEditor/DUNGEditor.cs
@@ -331,7 +331,7 @@ namespace DigimonWorld2Tool.FileEditor
                 #endregion
 
                 #region ScriptID
-                int scriptIDValue = (int)floorHeaderWindow.ScriptID0NumericUpDown.Value;
+                uint scriptIDValue = (uint)floorHeaderWindow.ScriptIDNumericUpDown.Value;
                 byte[] scriptIDData = BitConverter.GetBytes(scriptIDValue);
                 int scriptIDPointer = LoadedDUNGData.DungFloorHeaders[FloorIndex].DomainFloorBasePointer + (int)DungFloorHeader.DomainDataHeaderOffset.ScriptID;
                 Array.Copy(scriptIDData, 0, LoadedDUNGData.RawFileData, scriptIDPointer, scriptIDData.Length);
